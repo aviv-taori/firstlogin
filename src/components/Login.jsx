979e76c37da3c00364d3a238e8f7d3b10898482f
aviv-taori <134504658+aviv-taori@users.noreply.github.com>
2023-11-07 16:17:17 +0200

    part 1

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <h1>Hello</h1>
+        <input type='text' placeholder='What is your name?' />
+        <button>Submit</button>
+    </div>
+  )
+}
+
+export default Login
