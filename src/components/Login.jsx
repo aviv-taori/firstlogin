a320e03a94b0e50bde6c40384758b9a6d8345d50
aviv-taori <134504658+aviv-taori@users.noreply.github.com>
2023-11-08 10:23:21 +0200

    Part 2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { useState } from "react"
+
 
 function Login() {
+    const [name, setName] = useState("")
+    const writeName = event => {setName(event.target.value) ,setNewName(false) }
+
+    const [newName, setNewName] = useState(false)
+    const nameClack = () => setNewName(true)
+
+
+
+    const [onMouse, setOnMuose] = useState(false)
+    const changeOn = () => setOnMuose(true)
+    const changeOut = () => setOnMuose(false)
+
   return (
     <div>
-        <h1>Hello</h1>
-        <input type='text' placeholder='What is your name?' />
-        <button>Submit</button>
+    
+        <h1>Hello {newName && name}</h1>
+    
+        <input
+         type='text'
+          placeholder='What is your name?'
+          value={name}
+          onChange={writeName}
+           />
+        <button
+        onClick={nameClack}
+        onMouseOver={changeOn} 
+        onMouseOut={changeOut}
+        style={{backgroundColor: onMouse ? 'black': 'white' , color: "red"}}
+        >Submit
+        </button>
+        
     </div>
   )
 }
