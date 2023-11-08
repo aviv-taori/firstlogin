f75433a0e081200b9e7ef9e96009e3130a0e74a1
aviv-taori <134504658+aviv-taori@users.noreply.github.com>
2023-11-08 12:09:50 +0200

    Part 3

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,40 +1,51 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 function Login() {
-    const [name, setName] = useState("")
-    const writeName = event => {setName(event.target.value) ,setNewName(false) }
+  const [fName, setFName] = useState("");
+  const writeFName = (event) => {
+    setFName(event.target.value);
+  };
 
-    const [newName, setNewName] = useState(false)
-    const nameClack = () => setNewName(true)
+  const [lName, setLName] = useState("");
+  const writeLName = (event) => {
+    setLName(event.target.value);
+  };
+  const nameClack = (event) => { event.preventDefault();
+  };
 
-
-
-    const [onMouse, setOnMuose] = useState(false)
-    const changeOn = () => setOnMuose(true)
-    const changeOut = () => setOnMuose(false)
+  const [onMouse, setOnMuose] = useState(false);
+  const changeOn = () => setOnMuose(true);
+  const changeOut = () => setOnMuose(false);
 
   return (
     <div>
-    
-        <h1>Hello {newName && name}</h1>
-    
+      <h1>Hello { fName } { lName } </h1>
+      <form onSubmit={nameClack}>
         <input
-         type='text'
-          placeholder='What is your name?'
-          value={name}
-          onChange={writeName}
-           />
+          type="text"
+          placeholder="First name"
+          value={fName}
+          onChange={writeFName}
+        />
+
+        <input
+          type="text"
+          placeholder="Last name"
+          value={lName}
+          onChange={writeLName}
+        />
+
         <button
-        onClick={nameClack}
-        onMouseOver={changeOn} 
-        onMouseOut={changeOut}
-        style={{backgroundColor: onMouse ? 'black': 'white' , color: "red"}}
-        >Submit
+          type="sumit"
+          onMouseOver={changeOn}
+          onMouseOut={changeOut}
+          style={{ backgroundColor: onMouse ? "black" : "white", color: "red" }}
+        >
+          Submit
         </button>
-        
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
